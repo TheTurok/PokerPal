144dff72052cfe1b69e6ebd7b46c3be45dec03f2
airokay <airokairok@gmail.com>
2016-07-15 00:49:22 -0700

    General Layout Complete Further layout will not be needed until the game is functional

--- app/src/main/java/givememoney/pokerpal/pokergame.java
+++ app/src/main/java/givememoney/pokerpal/pokergame.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+/** google doc for PokerPal
+ *      http://tinyurl.com/thepokerpal
+ */
 
 public class pokergame extends Activity {
 
