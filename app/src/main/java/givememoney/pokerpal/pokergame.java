519073cd43f80369d96e1add84e5d1be63557fdf
jalbatross <ih8u2o@gmail.com>
2016-08-06 22:28:15 -0700

    Added basic BET functionality on click.

--- app/src/main/java/givememoney/pokerpal/pokergame.java
+++ app/src/main/java/givememoney/pokerpal/pokergame.java
@@ -1,22 +1,89 @@
 package givememoney.pokerpal;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /** google doc for PokerPal
  *      http://tinyurl.com/thepokerpal
  */
 
+
+
 public class pokergame extends Activity {
 
+    private Button betButton;
+    private TextView potString;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pokergame);
+
+        //Gets correct things from activity_pokergame.xml
+        betButton = (Button) findViewById(R.id.bet);
+        potString = (TextView) findViewById(R.id.potnumber);
+
+        betButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                showBetInputDialog();
+
+            }
+        });
+
     }
 
+    //Creates an input dialog for user BET on click of BET button
+    //TODO: Add greater functionality (bet X amount of big blinds)
+    //TODO: Raise, reraise, pot size bet, x2, etc.
+    protected void showBetInputDialog() {
+
+        // get prompts.xml view
+        LayoutInflater layoutInflater = LayoutInflater.from(pokergame.this);
+        View promptView = layoutInflater.inflate(R.layout.prompts, null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(pokergame.this);
+        alertDialogBuilder.setView(promptView);
+
+        final EditText editText = (EditText) promptView.findViewById(R.id.betInput);
+        // setup a dialog window
+        alertDialogBuilder.setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        //Get user input and current pot as string
+                        String inputToString = editText.getText().toString();
+                        String currentPot = potString.getText().toString();
+
+                        //Turn them into ints, then calculate pot
+                        Double betAmount = Double.parseDouble(inputToString);
+                        Double potAmount = Double.parseDouble(currentPot);
+                        Double finalAmount = betAmount+potAmount;
+                        String finalPot = Double.toString(finalAmount);
+
+                        potString.setText(finalPot);
+                    }
+                })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+        // create an alert dialog
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
