44edad6c610f46e71c6369b8fd37e3101df9949c
jalbatross <ih8u2o@gmail.com>
2016-08-09 12:14:29 -0700

    Added Player and PokerTable classes  -Test for minBet is present in pokergame.java, based on current  player stack size. Player can't bet more than their chips  -Some idea for layout of PokerTable class

---
+++ app/src/main/java/givememoney/table/Player.java
@@ -0,0 +1,50 @@
+package givememoney.table;
+
+/**
+ * Basic implementation of a Player object
+ * Created by Joey on 08/09/2016.
+ */
+
+
+
+public class Player {
+    private double m_totalCash;
+    public static enum Status {
+        ACTIVE, WAITING, IDLE
+    }
+    private Status m_status;
+    private boolean m_checkFold;
+    public String m_name;
+
+    public Player() {
+        m_totalCash = 100;
+        init();
+        m_name = "Khris Go";
+    }
+
+    public Player(double startingCash) {
+        if (startingCash < 0)
+            startingCash = 0;
+        m_totalCash = startingCash;
+        m_name = "Khris Go";
+        init();
+    }
+
+    public Player(double startingCash, String name) {
+        if (startingCash < 0)
+            startingCash = 0;
+        m_totalCash = startingCash;
+        m_name = name;
+        init();
+    }
+
+    private void init() {
+        m_status = Status.WAITING;
+        m_checkFold = false;
+    }
+
+    public Status getStatus(){ return m_status;}
+    public double getCash() { return m_totalCash;}
+    public String m_name() { return m_name;}
+
+}
---
+++ app/src/main/java/givememoney/table/PokerTable.java
@@ -0,0 +1,12 @@
+package givememoney.table;
+
+/**
+ * This class defines the Table object. Initialized upon starting a game?
+ * Created by Joey on 08/07/2016.
+ */
+public class PokerTable {
+    public static int m_numPlayers;
+    public static Player[] Players;
+
+}
+
