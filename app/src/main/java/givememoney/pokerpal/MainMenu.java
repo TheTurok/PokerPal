2cbe675f8feecd5c30eea645456a2742557c651d
jalbatross <ih8u2o@gmail.com>
2016-08-11 22:03:36 -0700

    Added MainMenu functionality and basic layout   -MainMenu is now the default starting activity   -Creates a pokergame activity after clicking New Game

--- app/src/main/java/givememoney/pokerpal/MainMenu.java
+++ app/src/main/java/givememoney/pokerpal/MainMenu.java
@@ -1,7 +1,10 @@
 package givememoney.pokerpal;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainMenu extends Activity {
 
@@ -9,6 +12,14 @@ public class MainMenu extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void startPokerGame(View v) {
+        Intent intent = new Intent(this, pokergame.class);
+        //EditText editText = (EditText) findViewById(R.id.edit_message);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 
 }
