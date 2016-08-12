28cb384958b719914af94ddc33b32a502e3fa3a0
jalbatross <ih8u2o@gmail.com>
2016-08-11 21:49:09 -0700

    Created MainMenu activity  -Minor fixes for strings in prompts.xml  -Added betInput string in strings.xml

---
+++ app/src/main/java/givememoney/pokerpal/MainMenu.java
@@ -0,0 +1,14 @@
+package givememoney.pokerpal;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainMenu extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+}
