8ad52fc7037ee287560fb9b1de7f9174ff456a80
Timothy Choi <tchoi@ucsd.edu>
2016-08-16 14:41:37 -0700

    InGameOtion Resume and MainMenu btn finished.

--- app/src/main/java/givememoney/pokerpal/IngameOption.java
+++ app/src/main/java/givememoney/pokerpal/IngameOption.java
@@ -1,7 +1,11 @@
 package givememoney.pokerpal;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import org.greenrobot.eventbus.EventBus;
 
 public class IngameOption extends Activity {
 
@@ -10,4 +14,14 @@ public class IngameOption extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingame_option);
     }
+
+    public void goToMainMenu(View v) {
+        Intent intent = new Intent(this, MainMenu.class);
+        startActivity(intent);
+    }
+
+    public void goResume(View v) {
+        finish();
+    }
+
 }
