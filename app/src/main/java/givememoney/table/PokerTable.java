29ec3985b23a7c49de305158ad8d973913fb1010
jalbatross <ih8u2o@gmail.com>
2016-08-22 15:15:30 -0700

    Merge conflict tester

--- app/src/main/java/givememoney/table/PokerTable.java
+++ app/src/main/java/givememoney/table/PokerTable.java
@@ -4,7 +4,9 @@ package givememoney.table;
  * This class defines the Table object. Initialized upon starting a game?
  * Created by Joey on 08/07/2016.
  */
+
 public class PokerTable {
+    public int penis;
     public static int m_numPlayers;
     public static Player[] Players;
 
